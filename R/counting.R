fd8687726fa037d00de0d668201a0be8e8f3c56c
Nicholas <nicholasrazalii@gmail.com>
2023-03-16 14:43:37 +0700

    fixed counting

--- R/counting.R
+++ R/counting.R
@@ -1,5 +1,9 @@
-# examples
-#
-counting <- function(x=1, y=2){
+
+#' Creates counting
+#' @export
+#' @example
+#' @param x numeric variable
+#' @param y numeric variable
+counting <- function(x, y){
   sum(x,y)
 }
