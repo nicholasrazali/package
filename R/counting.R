a5c6ee1802ebddbbf9819c08e5b6a5dc50143051
Nicholas <nicholasrazalii@gmail.com>
2023-03-17 00:11:42 +0700

    add detail and example in description

--- R/counting.R
+++ R/counting.R
@@ -1,9 +1,22 @@
 
 #' Creates counting
+#' @description
+#' `sum` returns the sum of all the values present in its arguments.
+#'
+#' @details
+#' This is a generic function: methods can be defined for it directly
+#' or via the [Summary()] group generic. For this to work properly,
+#' the arguments `...` should be unnamed, and dispatch is on the
+#' first argument.
 #' @export
-#' @example
+#' @examples
+#' counting(10, 3)
+#' counting(20, 5)
 #' @param x numeric variable
 #' @param y numeric variable
 counting <- function(x, y){
-  sum(x,y)
+
+  sum = x+y
+  min = x-y
+  return(list("sum" = sum, "minus" = min))
 }
